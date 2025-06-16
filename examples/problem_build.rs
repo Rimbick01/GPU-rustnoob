9617dd1b2d16c9242d69257ea85b14d22d3a9da3
rimbick01 <tushard.pe.ug@jadavpuruniversity.in>
2025-06-16 09:32:17 +0530

    multi kernel file

---
+++ examples/problem_build.rs
@@ -0,0 +1,52 @@
+use ocl::{ enums::{ProgramBuildInfo, ProgramBuildInfoResult}};
+use ocl::builders::ProgramBuilder;
+use ocl::{Context, Device, DeviceType,  Platform};
+use std::fs;
+
+
+const PROGRAM_FILE: &str = "good.cl";
+const PROGRAM_FILE_1: &str = "bad.cl";
+const OPTIONS: &str = "-cl-finite-math-only -cl-no-signed-zeros";
+
+fn main() -> ocl::Result<()> {
+
+    let platforms = Platform::list();
+
+    let platform = platforms.into_iter().next().expect("No platforms found");
+
+    let mut devices = Device::list(platform, Some(DeviceType::GPU)).unwrap_or_default();
+    if devices.is_empty() {
+        devices = Device::list(platform, Some(DeviceType::CPU)).unwrap_or_default();
+    }
+    let dev = devices.into_iter().next().expect("No devices found");
+
+    let context = Context::builder()
+        .platform(platform)
+        .devices(dev)
+        .build()?;
+        // .expect("Failed to create context");
+    let file_names = [PROGRAM_FILE, PROGRAM_FILE_1];
+    let mut program_sources = Vec::<String>::new();
+    for file_name in &file_names {
+        let program_handle = fs::read_to_string(file_name).unwrap_or_else(|_| panic!("Failed to read file: {}", file_name));
+        program_sources.push(program_handle);
+    }
+
+    let program_con = ProgramBuilder::new()
+        .src(program_sources[0].as_str())
+        .src(program_sources[1].as_str())
+        .devices(dev.clone())
+        .cmplr_opt(&*OPTIONS)
+        .build(&context)
+        .unwrap(); // <-- Now program is a Program, not a Result
+
+
+    let log = match program_con.build_info(dev, ProgramBuildInfo::BuildLog) {
+        Ok(ProgramBuildInfoResult::BuildLog(s)) => s,
+        _ => "<No build log available>".to_string(),
+    };
+    println!("hi log {}", log);
+
+    println!("Current directory: {:?}", std::env::current_dir());
+    Ok(())
+}
